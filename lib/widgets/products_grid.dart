c9ee07ec35e699d8b0a175b39af2f0734d9b191d
David Pam <niratpam@gmail.com>
2021-06-13 05:09:46 +0100

    Exploring Alternative Provider Syntaxes

--- lib/widgets/products_grid.dart
+++ lib/widgets/products_grid.dart
@@ -12,13 +12,13 @@ class ProductsGrid extends StatelessWidget {
     return GridView.builder(
       padding: const EdgeInsets.all(10.0),
       itemCount: products.length,
-      itemBuilder: (ctx, i) => ChangeNotifierProvider(
-        create: (c) => products[i],
+      itemBuilder: (ctx, i) => ChangeNotifierProvider.value(
+        value: products[i],
         child: ProductItem(
-          // products[i].id,
-          // products[i].title,
-          // products[i].imageUrl,
-        ),
+            // products[i].id,
+            // products[i].title,
+            // products[i].imageUrl,
+            ),
       ),
       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
         crossAxisCount: 2,
