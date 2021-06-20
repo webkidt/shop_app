a09a425b1b3024d8061e4cf70fe4d8b0cf7bb7cc
David Pam <niratpam@gmail.com>
2021-06-20 07:25:14 +0100

    Adding Shopping Cart Data

---
+++ lib/providers/cart.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/foundation.dart';
+
+import './cart_item.dart';
+
+class Cart with ChangeNotifier {
+  Map<String, CartItem> _items;
+
+  Map<String, CartItem> get items {
+    return {..._items};
+  }
+
+  void addItem(String productId, double price, String title) {
+    if (_items.containsKey(productId)) {
+      // change quantity
+      _items.update(
+        productId,
+        (existingCartItem) => CartItem(
+          id: existingCartItem.id,
+          title: existingCartItem.title,
+          price: existingCartItem.price,
+          quantity: existingCartItem.quantity + 1,
+        ),
+      );
+    } else {
+      _items.putIfAbsent(
+        productId,
+        () => CartItem(
+          id: DateTime.now().toString(),
+          title: title,
+          price: price,
+          quantity: 1,
+        ),
+      );
+    }
+  }
+}
